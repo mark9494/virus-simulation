c963b5751931af250bbad9f9ec722ffed1cecfc8
Anthony <anthonyrobot@hotmail.com>
2022-10-04 20:30:29 -0400

    Adding project to main

---
+++ virus-simulation/app/src/main/java/edu/vanier/virussimulation/cells/RecoveredCells.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.virussimulation.cells;
+
+
+
+public class RecoveredCells extends Cells {
+  
+    
+    public RecoveredCells( double size, double speed, boolean motion){
+      super(size, speed, motion);
+       
+        
+        
+    }
+    
+    
+    
+}
