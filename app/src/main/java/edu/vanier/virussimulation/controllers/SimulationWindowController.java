dd3c0e2ae04f33bdc39ad62e53d77b8cc31d305f
2125881 <2125881@d24237-548xth3.VanierCollege.intra>
2022-10-18 14:14:39 -0400

    moving circle added

--- app/src/main/java/edu/vanier/virussimulation/controllers/SimulationWindowController.java
+++ app/src/main/java/edu/vanier/virussimulation/controllers/SimulationWindowController.java
@@ -4,14 +4,49 @@
  */
 package edu.vanier.virussimulation.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
 /**
  *
  * @author Mark
  */
 public class SimulationWindowController {
     
+    
+         @FXML
+         private Group g;
+   
     public SimulationWindowController(){
         
     }
+    
+  public void handleStart(){
+     
+     final Circle c = new Circle(10);
+     c.setFill(Color.BLUE);
+     g.getChildren().addAll(c);
+     
+     
+     final Timeline timeline = new Timeline();
+     timeline.setCycleCount(Timeline.INDEFINITE);
+     
+     final KeyValue keyValue1 = new KeyValue(c.centerXProperty(),300);
+     final KeyValue keyValue2 = new KeyValue(c.centerYProperty(),300);
+     
+     
+    final KeyFrame firstKeyFrame = new KeyFrame(Duration.millis(2000),keyValue1);
+    final KeyFrame secondKeyFrame = new KeyFrame(Duration.millis(2000),keyValue2);
+    timeline.getKeyFrames().add(firstKeyFrame);
+     timeline.getKeyFrames().add(secondKeyFrame);
+     timeline.play();
+     
+  }
     
 }
