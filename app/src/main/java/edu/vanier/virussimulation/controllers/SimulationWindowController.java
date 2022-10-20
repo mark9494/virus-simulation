7c4f6467a5c5f69491caeee95a2b9edf1b99aa14
2125881 <2125881@d20703-g3cxth3.VanierCollege.intra>
2022-10-20 14:17:05 -0400

    changed the group container into a pane

--- app/src/main/java/edu/vanier/virussimulation/controllers/SimulationWindowController.java
+++ app/src/main/java/edu/vanier/virussimulation/controllers/SimulationWindowController.java
@@ -9,6 +9,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
@@ -21,32 +22,39 @@ public class SimulationWindowController {
     
     
          @FXML
-         private Group g;
-   
+         private Pane pane;
+         Circle circle;
     public SimulationWindowController(){
         
     }
     
   public void handleStart(){
      
-     final Circle c = new Circle(10);
-     c.setFill(Color.BLUE);
-     g.getChildren().addAll(c);
+     circle = new Circle(10);
+     circle.setFill(Color.BLUE);
+     pane.getChildren().addAll(circle);
      
      
      final Timeline timeline = new Timeline();
      timeline.setCycleCount(Timeline.INDEFINITE);
      
-     final KeyValue keyValue1 = new KeyValue(c.centerXProperty(),620);
-     final KeyValue keyValue2 = new KeyValue(c.centerYProperty(),200);
+     final KeyValue keyValue1 = new KeyValue(circle.centerXProperty(),620);
+     final KeyValue keyValue2 = new KeyValue(circle.centerYProperty(),200);
      
-     
+    
     final KeyFrame firstKeyFrame = new KeyFrame(Duration.millis(2000),keyValue1);
     final KeyFrame secondKeyFrame = new KeyFrame(Duration.millis(2000),keyValue2);
     timeline.getKeyFrames().add(firstKeyFrame);
      timeline.getKeyFrames().add(secondKeyFrame);
      timeline.play();
      
+//     if(c.getBoundsInParent().intersects(g.getBoundsInParent())){
+//           System.out.println("collision");   
+//       } 
+     
+       
+     
   }
+  
     
 }
